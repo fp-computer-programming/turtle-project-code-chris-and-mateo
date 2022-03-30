65a363aba668b0c0c3b6db6a817877321efda77c
p22mbonilla <p22mbonilla@fairfieldprep.org>
2022-03-30 10:20:59 -0400

    add file

--- turtle_project.py
+++ turtle_project.py
@@ -15,7 +15,25 @@ steven = turtle.Turtle()
 window.setup(1000, 1000)
 window.screensize(1000, 1000)
 
-# Player Input of Shape of Turtle and Color
+# Player 1 Input of Shape of toby and Color
+def change_color_p1(color):
+    toby.pencolor(color)
+def change_shape_p1(shape):
+    toby.shape(shape)
+
+change_color_p1(window.textinput("color", "enter a color for Player 1"))
+change_shape_p1(window.textinput("shape", "enter a shape for Player 1"))
+
+# Player 2 Input of shape of steven and Color
+def change_color_p2(color):
+    steven.pencolor(color)
+def change_shape_p2(shape):
+    steven.shape(shape)
+
+change_color_p2(window.textinput("color", "enter a color for Player 1"))
+change_shape_p2(window.textinput("shape", "enter a shape for Player 1"))
+
+
 
 
 # Begin and end filling
