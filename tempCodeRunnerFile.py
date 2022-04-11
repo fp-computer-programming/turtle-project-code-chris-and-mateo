efb7ea7887259582ba1651f3e12837e38c184b31
p22rlugo <p22rlugo@fairfieldprep.org>
2022-04-11 02:39:57 -0400

    Turtle fin

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,19 @@
+def change_color_p1(color):
+#     toby.pencolor(color)
+#     toby.fillcolor(color)
+# def change_shape_p1(shape):
+#     toby.shape(shape)
+
+# change_color_p1(window.textinput("color", "enter a color for Player 1"))
+# change_shape_p1(window.textinput("shape", "enter a shape for Player 1"))
+
+# # Player 2 Input of shape of steven and Color
+# def change_color_p2(color):
+#     steven.fillcolor(color)
+#     steven.pencolor(color)
+# def change_shape_p2(shape):
+#     steven.shape(shape)
+
+
+# change_color_p2(window.textinput("color", "enter a color for Player 1"))
+# change_shape_p2(window.textinput("shape", "enter a shape for Player 1"))
--- turtle_project.py
+++ turtle_project.py
@@ -1,6 +1,7 @@
 # Authors: CRS and MB and RJL 03/28/22
 # Import turtle
 import turtle
+from random import randrange
 from threading import Timer
 
 # Set up for tody to fill in shapes
@@ -9,6 +10,10 @@ toby_table = []
 # Set up for steven to fill in shapes
 steven_table = []
 
+# Powerup Table
+powerUpX = []
+powerUpY = []
+
 # Claimed areas
 claimed = []
 
@@ -16,26 +21,51 @@ claimed = []
 window = turtle.Screen()
 
 # Turtles
+tobyP = turtle.Turtle()
+stevenP = turtle.Turtle()
+endTurtle = turtle.Turtle()
 toby = turtle.Turtle()
 steven = turtle.Turtle()
-endTurtle = turtle.Turtle()
-window.setup(1000, 1000)
+powerUp = turtle.Turtle()
+
+# Window
+window.setup(1000,1000)
 window.screensize(1000, 1000)
+
+# Scoreboard
+tobyP.penup()
+tobyP.hideturtle()
+tobyP.setposition(-400,440)
+tobyP.write("TOBY: 0",font=("Vernanda",20,"normal"))
+
+stevenP.penup()
+stevenP.hideturtle()
+stevenP.setposition(300,440)
+stevenP.write("STEVEN: 0",font=("Vernanda",20,"normal"))
+
+# Vars
+moving = True
+canPowerUp = True
+notTaken = True
+steven_points = 0
+toby_points = 0
 
 #creates border
 bdr = turtle.Turtle()
 bdr.speed(0)
 bdr.penup()
-bdr.back(350)
+bdr.setposition(-475,-475)
 bdr.pendown()
-bdr.left(90)
-bdr.forward(350)
-bdr.right(90)
-for i in range(3):
-    bdr.forward(700)
-    bdr.right(90)
-bdr.forward(350)
+bdr.pencolor("purple")
+bdr.pensize(10)
+for side in range(4):
+    bdr.forward(950)
+    bdr.left(90)
 bdr.hideturtle()
+
+endTurtle.penup()
+endTurtle.setposition(-100,-100)
+endTurtle.hideturtle()
 
 # Player 1 Input of Shape of toby and Color
 def change_color_p1(color):
@@ -76,23 +106,62 @@ steven.pendown()
 
 # Collision
 def collision_p1():
+    global toby_points
+    global powerUp
+    global notTaken
     for x in range(len(toby_table)):
         if int(toby.xcor()) == toby_table[x][0] and int(toby.ycor()) == toby_table[x][1]:
             toby.end_fill()
             toby.begin_fill()
-        elif toby.xcor() > 450 or toby.ycor() > 400:
-            toby.back(10)
+            toby_points += 1
+            tobyPoints()
+        elif toby.xcor() > 450 or toby.ycor() > 450 or toby.xcor() < -450 or toby.ycor() < -450:
+            toby.right(180)
+            toby.forward(20)
+        try:
+            if toby.xcor() > powerUpX[0] and notTaken or toby.ycor > powerUpY[0] and notTaken:
+                notTaken = False
+                powerUp.hideturtle()
+                powerUp.pendown()
+                powerUp.begin_fill()
+                for x in range(4):
+                    powerUp.forward(250)
+                    powerUp.left(90)
+                powerUp.end_fill()
+                toby_points += 10
+        except:
+           print
 
 def collision_p2():
+    global steven_points
+    global powerUp
+    global notTaken
     for x in range(len(steven_table)):
         if int(steven.xcor()) == steven_table[x][0] and int(steven.ycor()) == steven_table[x][1]:
             steven.end_fill()
             steven.begin_fill()
-        elif steven.xcor() > 450 or steven.ycor() > 400:
-            steven.back(10)
+            steven_points += 1
+            stevenPoints()
+        elif steven.xcor() > 450 or steven.ycor() > 450 or steven.xcor() < -450 or steven.ycor() < -450:
+            steven.right(180)
+            steven.forward(20)
+        try:
+            if steven.xcor() > powerUpX[0] and notTaken or steven.ycor > powerUpY[0] and notTaken:
+                notTaken = False
+                powerUp.hideturtle()
+                powerUp.pendown()
+                powerUp.begin_fill()
+                for x in range(4):
+                    powerUp.forward(250)
+                    powerUp.left(90)
+                powerUp.end_fill()
+                steven_points += 10
+        except:
+           print
 
 def endGame():
-    turtle.Turtle.write("GAME OVER!")
+    global moving
+    moving = False
 
 # Define Left Player 2 Function
 def left_p2():
@@ -110,9 +179,41 @@ def left_p1():
 def right_p1():
     toby.right(90)
 
+def tobyPoints():
+    global toby_points
+    tobyP.reset()
+    tobyP.hideturtle()
+    tobyP.speed(0)
+    tobyP.penup()
+    tobyP.setposition(-400,440)
+    tobyP.write("TOBY: "+str(toby_points),font=("Vernanda",20,"normal"))
+
+def stevenPoints():
+    global steven_points
+    stevenP.reset()
+    stevenP.hideturtle()
+    stevenP.speed(0)
+    stevenP.penup()
+    stevenP.setposition(300,440)
+    stevenP.write("STEVEN: "+str(steven_points),font=("Vernanda",20,"normal"))
+
+def powerup():
+    global canPowerUp
+    global powerUp
+    if canPowerUp and toby_points > 5 or canPowerUp and steven_points > 5:
+        canPowerUp = False
+        powerUp.penup()
+        powerUp.turtlesize(1)
+        powerUp.shape(name="square")
+        x = randrange(-300,300,1)
+        y = randrange(-300,300,1)
+        powerUp.setposition(x,y)
+        powerUp.write("BOMB POWERUP!",font=("Vernanda",15,"normal"))
+        powerUpX.append(x)
+        powerUpY.append(y)
 
 def move():
-    moving = True
+    global moving
     while moving:
         toby.forward(5)
 
@@ -134,8 +235,18 @@ def move():
         collision_p2()
         steven_table.append([int(steven.xcor()),int(toby.ycor())])
 
+        powerup()
+
+    if toby_points > steven_points:
+        endTurtle.write("GAME OVER!\n"+"TOBY HAS WON!",font=("Vernanda",35,"normal"))
+    elif toby_points < steven_points:
+        endTurtle.write("GAME OVER!\n"+"STEVEN HAS WON!",font=("Vernanda",35,"normal"))
+    else:
+        endTurtle.write("GAME OVER!\n"+"NOBODY HAS WON!",font=("Vernanda",35,"normal"))
+    window.mainloop()
+
 # TIMER OMEGALUL
-t = Timer(interval=5.0,function=endGame)
+t = Timer(interval=30.0,function=endGame)
 t.start()
 
 move()
